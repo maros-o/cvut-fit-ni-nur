3d5b7c44813bf4b3d26c478482d6d3490ce5e3ad
maros-o <maros.mec@seznam.cz>
2024-10-23 11:21:28 +0200

    auto commit & push

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function HomePage() {
   return (
     <div className="flex justify-center items-center h-screen">
       <nav className="flex flex-col gap-6 p-2 w-full max-w-xs">
-        <Button className="bg-green-600 border h-12" asChild>
+        <Button className="bg-green-600 border" asChild>
           <Link href="/test">Začít test</Link>
         </Button>
 
-        <Button variant="secondary" className="border h-12" asChild>
+        <Button variant="secondary" className="border" asChild>
           <Link href="/settings">Nastavení</Link>
         </Button>
       </nav>
