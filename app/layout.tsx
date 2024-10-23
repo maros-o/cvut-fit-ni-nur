c263edd14814fa874ff97f2a045eda5f17e078ef
maros-o <maros.mec@seznam.cz>
2024-10-24 00:28:27 +0200

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { SettingsProvider } from "./_contexts/SettingsContext";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -34,6 +35,7 @@ export default function RootLayout({
             {children}
           </main>
         </SettingsProvider>
+        <Analytics />
       </body>
     </html>
   );
