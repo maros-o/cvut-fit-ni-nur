c7679bbe95735d7eb808531fecfd0836ff20384c
maros-o <maros.mec@seznam.cz>
2024-10-27 11:52:23 +0100

    a

--- app/_mock_data/movies.ts
+++ app/_mock_data/movies.ts
@@ -4,7 +4,6 @@ export type Movie = {
   datetime: string;
   tags: string[];
   thumbnail_url: string;
-  length_min?: number;
 };
 
 const movies: Movie[] = [
@@ -12,8 +11,7 @@ const movies: Movie[] = [
     title: "Transformers: Probuzení monster",
     day: "Pátek",
     datetime: "20.12. 20:00",
-    tags: ["3D", "Titulky", "Akční", "Sci-fi"],
-    length_min: 127,
+    tags: ["3D", "Titulky", "Akční", "Sci-Fi"],
     thumbnail_url:
       "https://image.pmgstatic.com/cache/resized/w663/files/images/film/posters/167/502/167502367_200hik.jpg",
   },
@@ -22,7 +20,6 @@ const movies: Movie[] = [
     day: "Sobota",
     datetime: "23.6. 14:30",
     tags: ["2D", "Dabing", "Animovaný", "Komedie"],
-    length_min: 97,
     thumbnail_url:
       "https://image.pmgstatic.com/cache/resized/w663/files/images/film/posters/168/714/168714795_o5bchr.jpg",
   },
@@ -31,9 +28,40 @@ const movies: Movie[] = [
     day: "Sobota",
     datetime: "8.9. 12:45",
     tags: ["2D", "Dabing", "Animovaný", "Komedie"],
-    length_min: 94,
     thumbnail_url:
       "https://image.pmgstatic.com/cache/resized/w663/files/images/film/posters/169/041/169041061_7kr8oh.jpg",
+  },
+  {
+    title: "Vlny",
+    day: "Čtvrtek",
+    datetime: "1.5. 18:45",
+    tags: ["2D", "Drama", "Historický", "Thriller"],
+    thumbnail_url:
+      "https://image.pmgstatic.com/cache/resized/w663/files/images/film/posters/169/002/169002978_123omn.jpg",
+  },
+  {
+    title: "Vetřelec: Romulus",
+    day: "Úterý",
+    datetime: "18.2. 20:30",
+    tags: ["2D", "Titulky", "Sci-Fi", "Horor"],
+    thumbnail_url:
+      "https://image.pmgstatic.com/cache/resized/w663/files/images/film/posters/169/002/169002121_kb858t.jpg",
+  },
+  {
+    title: "Úsměv 2",
+    day: "Neděle",
+    datetime: "18.2. 20:30",
+    tags: ["2D", "Titulky", "Horor"],
+    thumbnail_url:
+      "https://image.pmgstatic.com/cache/resized/w663/files/images/film/posters/169/265/169265673_9w9cxy.jpg",
+  },
+  {
+    title: "Venom: Poslední tanec",
+    day: "Neděle",
+    datetime: "18.2. 20:30",
+    tags: ["2D", "Titulky", "Akční", "Sci-Fi"],
+    thumbnail_url:
+      "https://image.pmgstatic.com/cache/resized/w663/files/images/film/posters/169/342/169342049_0lu5s0.jpg",
   },
 ];
 
