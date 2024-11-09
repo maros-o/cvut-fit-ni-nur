901698499cb8e8eef27483cc8c2f9375fa844dd8
maros-o <maros.mec@seznam.cz>
2024-11-09 10:24:26 +0100

    a

--- app/test/seats/_components/Temper.tsx
+++ app/test/seats/_components/Temper.tsx
@@ -2,6 +2,7 @@ import {
   ticketTypeToBgColor,
   ticketTypeToInfoText,
   ticketTypeToLabel,
+  ticketTypeToPrice,
 } from "@/app/_constats/ticket";
 import TestSessionContext, {
   TicketType,
@@ -11,7 +12,14 @@ import { useContext } from "react";
 export const Temper = () => {
   const { selectedPaletteType } = useContext(TestSessionContext);
 
-  if (selectedPaletteType === "eraser") return null;
+  const ticketType = selectedPaletteType as TicketType;
+
+  if (selectedPaletteType === "eraser")
+    return (
+      <div className="flex flex-col items-center font-semibold text-[14px]">
+        Mazací guma
+      </div>
+    );
 
   return (
     <div className="flex flex-col items-center ">
@@ -19,16 +27,17 @@ export const Temper = () => {
         <div className="w-5 h-6 bg-gray-100 border" />
         <div className="w-1 h-7 bg-gray-400" />
         <div
-          className={`w-[220px] h-8 flex items-center justify-center font-semibold text-[14px] ${
-            ticketTypeToBgColor[selectedPaletteType as TicketType]
-          }`}
+          className={`w-[220px] h-8 flex gap-1 items-center justify-center font-semibold text-[14px] ${ticketTypeToBgColor[ticketType]}`}
         >
-          {ticketTypeToLabel[selectedPaletteType as TicketType]}
+          {ticketTypeToLabel[ticketType]}{" "}
+          <span className="font-normal text-black/80">
+            {ticketTypeToPrice[ticketType]},-
+          </span>
         </div>
         <div className="w-3 h-9 bg-gray-300" />
       </div>
       <div className="text-xs text-black/80">
-        {ticketTypeToInfoText[selectedPaletteType as TicketType]}
+        {ticketTypeToInfoText[ticketType]}
       </div>
     </div>
   );
